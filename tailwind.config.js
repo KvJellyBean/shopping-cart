bfca387d3e74e199fda97fb98c428717269ebb8a
KvJellyBean <kevinnatan91@gmail.com>
2024-07-10 18:08:48 +0700

    feat: add tailwind's commit for colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      body: ["Inconsolata", '"Lucida Console"', "sans-serif"],
+      heading: ["Michroma", '"Lucida Console"', "sans-serif"],
+    },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      primary: "#3040D3",
+      text: "#000000",
+      background: "#FFFFFF",
+    },
   },
   plugins: [],
 };
