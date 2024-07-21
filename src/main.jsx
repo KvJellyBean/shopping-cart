ed949a102755a98b037e36d9af879b2d945fd9a4
KvJellyBean <kevinnatan91@gmail.com>
2024-07-22 05:02:06 +0700

    feat: add routing and the components

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,42 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
+import { LandingPage } from "./routes/LandingPage";
+import { Games } from "./routes/Games";
+import { About } from "./routes/About";
+import { Cart } from "./routes/Cart";
+import { ErrorPage } from "./routes/ErrorPage";
+import { GameDetail } from "./routes/GameDetail";
 import "./index.css";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+        index: true,
+      },
+      {
+        path: "/games",
+        element: <Games />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/games/:gameID",
+        element: <GameDetail />,
+      },
+    ],
+    errorElement: <ErrorPage />,
   },
 ]);
 
