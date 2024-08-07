b0c93d827adc83ccf7d1163efd07651f38ca10d3
KvJellyBean <kevinnatan91@gmail.com>
2024-08-07 09:50:50 +0700

    feat: add contact in the footer

---
+++ src/components/Footer/FooterContact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const FooterContact = () => {
+  return (
+    <div data-testid="footer-contact">
+      <h4 className="text-lg font-heading max-sm:text-[1rem] mb-2">
+        Contact Us
+      </h4>
+      <p>+1 234 567 8910</p>
+    </div>
+  );
+};
